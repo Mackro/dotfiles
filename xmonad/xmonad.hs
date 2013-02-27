68061724a808e1589bc68d3f7af9781f322a49a2
John Barbero Unenge <john.barbero.unenge@gmail.com>
2013-02-27 15:11:17 +0100

    Made some changes to the XMonad config.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,11 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Data.Ratio ((%))
 
 myTerminal = "gnome-terminal"
 
@@ -17,11 +19,18 @@ myFocusedBorderColor = "purple"
 
 myFocusFollowsMouse = False
 
+myLayoutHook = avoidStruts (smartBorders (tiled ||| Mirror tiled ||| Full))
+                where
+                    tiled   =   Tall nmaster delta ratio
+                    nmaster =   1       -- Number of windows in the master panel
+                    ratio   =   2%3     -- Percentage of the screen to increment by when resizing the window
+                    delta   =   5%100   -- Default portion of the screen occupied by the master panel
+
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig {
         manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig,
+        layoutHook = myLayoutHook, 
       	logHook = dynamicLogWithPP xmobarPP {
 		      ppOutput = hPutStrLn xmproc,
 		      ppTitle = xmobarColor "red" "" . shorten 50
