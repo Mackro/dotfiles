9470c83612eae24155501bd4e5a4620e829ae131
John Barbero Unenge <john.barbero.unenge@gmail.com>
2013-09-26 14:46:06 +0200

    Add keybinds

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,6 +8,9 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import Data.Ratio ((%))
 import XMonad.Hooks.SetWMName
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+import System.Exit
 
 myTerminal = "gnome-terminal"
 
@@ -21,8 +24,62 @@ myFocusedBorderColor = "purple"
 
 myFocusFollowsMouse = False
 
-myWorkspaces = ["1. vim","2. term","3. web","4","5","6","7","8","9"]
+myWorkspaces = ["1. vim","2. term","3. web"] ++ map show [4..9]
 
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+	-- launch a terminal
+	[ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+	-- launch dmenu
+	, ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu -b ` && eval \"exec $exe\"")
+	
+	-- Terminate application
+	, ((modm .|. shiftMask, xK_c     ), kill)
+
+	-- Switch layout
+    , ((modm,               xK_space ), sendMessage NextLayout)
+	
+	-- Reset xmonad conf
+	, ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    
+	-- Move focus to next
+	, ((modm,               xK_Tab   ), windows W.focusDown)
+	
+	-- Move focus to master
+	, ((modm,               xK_m     ), windows W.focusMaster  )
+
+	-- Swap the focused window and the master window
+	, ((modm,               xK_Return), windows W.swapMaster)
+
+	-- Shrink the master area
+	, ((modm,               xK_h     ), sendMessage Shrink)
+	
+	-- Expand the master area
+	, ((modm,               xK_l     ), sendMessage Expand)
+	
+	-- Push window back into tiling
+	, ((modm,               xK_t     ), withFocused $ windows . W.sink)
+
+	-- toggle the status bar gap (used with avoidStruts from Hooks.ManageDocks)
+	, ((modm , xK_b ), sendMessage ToggleStruts)
+      
+	-- Quit xmonad
+	, ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+	
+	-- Restart xmonad
+	, ((modm              , xK_q     ), restart "xmonad" True)
+	]
+	++
+	-- mod-[1..9], Switch to workspace N
+	-- mod-shift-[1..9], Move client to workspace N
+	[((m .|. modm, k), windows $ f i)
+		| (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+	    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+	++
+	-- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+	-- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+	[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+		| (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+		, (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 myLayoutHook = avoidStruts (smartBorders (tiled ||| Mirror tiled) ||| noBorders(Full)) ||| noBorders (fullscreenFull Full) 
                 where
@@ -51,6 +108,7 @@ main = do
 		terminal = myTerminal,
 		modMask = myModMask,
 		workspaces = myWorkspaces,
+		keys = myKeys,
         borderWidth = myBorderWidth,
 		startupHook = myStartupHook,
         normalBorderColor = myNormalBorderColor,
